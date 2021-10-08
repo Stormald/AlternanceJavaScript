6457eae812c5e891bc1b3d525cf04e54a59f9d29
Thomas Gomes Da Silva <t.gds74@gmail.com>
2021-10-08 13:39:18 +0200

    Ajout calculatrice à une opération

---
+++ scripts/calculatrice.js
@@ -0,0 +1,32 @@
+let ajouterChiffre = (num) =>{
+    let calcul = document.querySelector("#calcul");
+    calcul.value+=num;
+    console.log(num);
+}
+
+let ajouterOperateur = (ope) =>{
+    let calcul = document.querySelector("#calcul");
+    calcul.value+=ope;
+    console.log(ope);
+}
+
+let calculer = (calcul) =>{
+    let nombresArray = [];
+    let resultat = document.querySelector("#resultat");
+    if(calcul.includes("+")){
+        nombresArray = calcul.split("+");
+        resultat.value = +nombresArray[0] + +nombresArray[1];
+    } else if(calcul.includes("-")){
+        nombresArray = calcul.split("-");
+        resultat.value = +nombresArray[0] - +nombresArray[1];
+    } else if(calcul.includes("/")){
+        nombresArray = calcul.split("/");
+        resultat.value = +nombresArray[0] / +nombresArray[1];
+    } else if(calcul.includes("x")){
+        nombresArray = calcul.split("x");
+        resultat.value = +nombresArray[0] * +nombresArray[1];
+    }
+    else{
+        resultat.value ="Opérateur incorrect";
+    }
+}
