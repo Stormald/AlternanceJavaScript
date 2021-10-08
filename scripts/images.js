f6a01371e104adb1af7a10fdda321d11d9d93c33
Thomas Gomes Da Silva <t.gds74@gmail.com>
2021-10-08 11:07:20 +0200

    Ajout images rotation qui fonctionne

--- scripts/images.js
+++ scripts/images.js
@@ -1,2 +1,14 @@
+let bouger = () => {
+    let images = document.querySelectorAll("img");
+    let imgSrcIntial= [];
 
-let images = document.querySelectorAll("img");
+    for (let index = 0; index < images.length; index++) {
+        imgSrcIntial.push(images[index].src);
+    }
+
+    images[0].src = imgSrcIntial[2];
+    images[1].src = imgSrcIntial[0];
+    images[2].src = imgSrcIntial[3];
+    images[3].src = imgSrcIntial[1];
+}
+
